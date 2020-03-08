4acc467f70f20e5eb624dc40ba94ab9bc89ae178
Jonathan de Toni <jonathan@tonidev.com.br>
2020-03-08 20:46:50 -0300

    fix(database): Correção da string de conexão.

--- Api.Data/Context/ContextFactory.cs
+++ Api.Data/Context/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace Api.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;Database=Jobis;Uid=jonathandetoni;Pwd=Ton!960427";
+            var connectionString = "Server=localhost;Port=3306;Database=dbapi;Uid=root;Pwd=root";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
