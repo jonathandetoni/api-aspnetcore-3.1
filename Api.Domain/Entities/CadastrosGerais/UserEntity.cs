2855e1789047b0f447213a22f9c58efa9da46b27
Jonathan de Toni <jonathan@tonidev.com.br>
2020-03-08 20:36:43 -0300

    Inital Commit.

---
+++ Api.Domain/Entities/CadastrosGerais/UserEntity.cs
@@ -0,0 +1,8 @@
+namespace Api.Domain.Entities.CadastrosGerais
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
